7180b2ec86dd2c08b166807d5b29a81fda6068ca
mschi <mschiffke@hotmail.de>
2021-07-27 01:33:01 +0200

    Added headlines and styling

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 st.write("""
 # Simple Stock Price Viewer Web App 
 
-Shown are the stock closing price and volume of Intellia Therapeutics!
+Shown are the stock **closing** price and ***volume*** of Intellia Therapeutics!
 
 """)
 
@@ -18,7 +18,14 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-6-26')
 # Open High      Low Close     Volume Dividends Stock Splits
 
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume
+""")
 st.line_chart(tickerDf.Volume)
 
 
